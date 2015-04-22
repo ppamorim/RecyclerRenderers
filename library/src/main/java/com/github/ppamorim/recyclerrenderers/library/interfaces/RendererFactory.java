360b070c4d38a3481ce25a4c87aa42dae390cc77
ppamorim <pp.amorim@hotmail.com>
2015-04-22 11:18:37 -0300

    library removed

--- library/src/main/java/com/github/ppamorim/recyclerrenderers/library/interfaces/RendererFactory.java
+++
@@ -1,7 +0,0 @@
-package com.github.ppamorim.recyclerrenderers.library.interfaces;
-
-import com.github.ppamorim.recyclerrenderers.library.renderer.Renderer;
-
-public interface RendererFactory {
-  Renderer getRenderer(int id);
-}
