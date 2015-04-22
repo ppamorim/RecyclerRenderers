193f8ab092debd8da348258b0db84d0897fd32ef
ppamorim <pp.amorim@hotmail.com>
2015-04-22 11:17:38 -0300

    rename

---
+++ recyclerrenderers/src/main/java/com/github/ppamorim/recyclerrenderers/library/adapter/RendererAdapter.java
@@ -0,0 +1,80 @@
+package com.github.ppamorim.recyclerrenderers.library.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+import com.github.ppamorim.recyclerrenderers.library.builder.RendererBuilder;
+import com.github.ppamorim.recyclerrenderers.library.interfaces.Builder;
+import com.github.ppamorim.recyclerrenderers.library.interfaces.Renderable;
+import com.github.ppamorim.recyclerrenderers.library.renderer.Renderer;
+import com.github.ppamorim.recyclerrenderers.library.viewholder.RenderViewHolder;
+import java.util.Collection;
+import java.util.List;
+
+public class RendererAdapter extends RecyclerView.Adapter<RenderViewHolder> {
+
+  private List<Renderable> items;
+  private Builder builder;
+
+  public RendererAdapter(Collection<? extends Renderable> items, RendererBuilder builder) {
+    this((List<? extends Renderable>)items, builder);
+  }
+
+  public RendererAdapter(List<? extends Renderable> items, RendererBuilder builder) {
+    this.items = (List<Renderable>) items;
+    if (items == null) {
+      throw new IllegalArgumentException("Data must not be null");
+    }
+    this.builder = builder;
+    if (builder == null) {
+      throw new IllegalArgumentException("Builder must not be null");
+    }
+  }
+
+  @Override public RenderViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+    Renderer renderer = builder.instantiate(viewType).create();
+    return renderer.onCreateViewHolder(viewGroup, viewType);
+  }
+
+  @Override public void onBindViewHolder(RenderViewHolder holder, int position) {
+    holder.onBindView(items.get(position));
+    holder.setItem(items.get(position));
+  }
+
+  @Override public long getItemId(int position) {
+    return super.getItemId(position);
+  }
+
+  @Override public void onViewRecycled(RenderViewHolder holder) {
+    super.onViewRecycled(holder);
+    holder.onViewRecycled();
+  }
+
+  @Override public void onViewAttachedToWindow(RenderViewHolder holder) {
+    super.onViewAttachedToWindow(holder);
+    holder.onViewAttachedToWindow();
+  }
+
+  @Override public void onViewDetachedFromWindow(RenderViewHolder holder) {
+    super.onViewDetachedFromWindow(holder);
+    holder.onViewDetachedFromWindow();
+  }
+
+  @Override public int getItemCount() {
+    return items.size();
+  }
+
+  @Override public int getItemViewType(int position) {
+    return items.get(position).getRenderableResourceId();
+  }
+
+  public void add(Renderable item, int position) {
+    items.add(position, item);
+    notifyItemInserted(position);
+  }
+
+  public void clear() {
+    items.clear();
+    notifyDataSetChanged();
+  }
+
+}
